5665670c8674ae45c7d3fc41a0eba45e826b4799
unknown <youakimj8031@my.uwstout.edu>
2020-09-19 13:55:32 -0500

    Got the mesh to follow the ragdoll
    For some reason the left arm is jittery, tried fixing to no avail. Still need to set limits on the joints to get the ragdoll to stand up

---
+++ Assets/Scripts/TheBoneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheBoneController : MonoBehaviour
+{
+    Transform ragdollBone;
+    public Transform targetedBone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ragdollBone = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetedBone.position = ragdollBone.position;
+        targetedBone.rotation = ragdollBone.rotation;
+    }
+}
