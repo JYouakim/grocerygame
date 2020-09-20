1bfb26c48d2e2fda5011fc028a5d30a615a32f5f
unknown <youakimj8031@my.uwstout.edu>
2020-09-20 13:54:43 -0500

    floppy boi
    The floppy man stands on his own now. Still an issue with his left arm twitching and he needs a physics material to be able to move around a bit better.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
+    public Camera mainCamera;
 
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
     private void FixedUpdate() {
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        Vector3 smoothedPosition = Vector3.Lerp(mainCamera.transform.position, desiredPosition, smoothSpeed);
+        mainCamera.transform.position = smoothedPosition;
 
-        transform.LookAt(target);
+        mainCamera.transform.LookAt(target);
     }
 }
